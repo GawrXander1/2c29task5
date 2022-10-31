e7ec026a157c24f4c6b15447d1e8f33d7f2b350d
GawrXander1 <115450405+GawrXander1@users.noreply.github.com>
2022-10-31 15:01:04 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let xvalue = 0
+let yvalue = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    xvalue = 0
+    yvalue = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(xvalue, yvalue)
+            basic.pause(500)
+            xvalue += 1
+        }
+        yvalue += 1
+        xvalue = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    xvalue = 0
+    yvalue = 0
+    for (let index = 0; index < 5; index++) {
+        led.plot(xvalue, yvalue)
+        basic.pause(500)
+        xvalue += 1
+    }
+    yvalue += 1
+    for (let index = 0; index < 5; index++) {
+        xvalue += -1
+        led.plot(xvalue, yvalue)
+        basic.pause(500)
+    }
+    xvalue = 0
+    yvalue += 1
+    for (let index = 0; index < 5; index++) {
+        led.plot(xvalue, yvalue)
+        basic.pause(500)
+        xvalue += 1
+    }
+    yvalue += 1
+    for (let index = 0; index < 5; index++) {
+        xvalue += -1
+        led.plot(xvalue, yvalue)
+        basic.pause(500)
+    }
+    xvalue = 0
+    yvalue += 1
+    for (let index = 0; index < 5; index++) {
+        led.plot(xvalue, yvalue)
+        basic.pause(500)
+        xvalue += 1
+    }
+})
+basic.forever(function () {
+	
+})
